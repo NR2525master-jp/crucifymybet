73d91e72710c6f2673439146ff2e286617945919
NR2525master-jp <nakaura2525nakaura@gmail.com>
2025-03-04 19:57:37 +0900

    本番デプロイ

--- myproject/settings.py
+++ myproject/settings.py
@@ -13,7 +13,7 @@ if not SECRET_KEY:
     raise ValueError("SECRET_KEY が設定されていません。環境変数を設定してください。")
 
 # 本番/開発切り替え
-DEBUG = os.getenv("DEBUG", "False") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "False"
 
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
 SECURE_SSL_REDIRECT = os.getenv("SECURE_SSL_REDIRECT", "False") == "True"
@@ -57,21 +57,21 @@ if DEBUG:
 
 
 # 本番環境の設定
-#SECURE_BROWSER_XSS_FILTER = True  # XSS対策
-#SECURE_CONTENT_TYPE_NOSNIFF = True  # MIME スニッフィング対策
-#SECURE_HSTS_SECONDS = 31536000  # HSTS（1年間）
-#SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # サブドメインも HSTS 適用
-#SECURE_HSTS_PRELOAD = True  # HSTS Preload 対応
-#SESSION_COOKIE_SECURE = True  # HTTPS のみでクッキー送信
-#CSRF_COOKIE_SECURE = True  # CSRF クッキーを HTTPS のみに制限
-#SECURE_SSL_REDIRECT = True  # HTTP から HTTPS へ強制リダイレクト
+SECURE_BROWSER_XSS_FILTER = True  # XSS対策
+SECURE_CONTENT_TYPE_NOSNIFF = True  # MIME スニッフィング対策
+SECURE_HSTS_SECONDS = 31536000  # HSTS（1年間）
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # サブドメインも HSTS 適用
+SECURE_HSTS_PRELOAD = True  # HSTS Preload 対応
+SESSION_COOKIE_SECURE = True  # HTTPS のみでクッキー送信
+CSRF_COOKIE_SECURE = True  # CSRF クッキーを HTTPS のみに制限
+SECURE_SSL_REDIRECT = True  # HTTP から HTTPS へ強制リダイレクト
 
 # 開発環境の設定
 # 開発環境用に一時的に無効化する（HTTPS リダイレクトをしない）
-SECURE_SSL_REDIRECT = False  # ローカルでのデバッグ用
-SECURE_HSTS_SECONDS = 3600  # 1時間（テスト用）
-SECURE_HSTS_INCLUDE_SUBDOMAINS = False  # サブドメインは適用しない
-SECURE_HSTS_PRELOAD = False  # プリロードリストに登録しない
+#SECURE_SSL_REDIRECT = False  # ローカルでのデバッグ用
+#SECURE_HSTS_SECONDS = 3600  # 1時間（テスト用）
+#SECURE_HSTS_INCLUDE_SUBDOMAINS = False  # サブドメインは適用しない
+#SECURE_HSTS_PRELOAD = False  # プリロードリストに登録しない
 
 # CORS の設定
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS", "False") == "True"
@@ -80,7 +80,14 @@ if not CORS_ALLOW_ALL_ORIGINS:
     CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",") if os.getenv("CORS_ALLOWED_ORIGINS") else []
 
 CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
-CORS_ALLOW_HEADERS = ["*"]
+#開発用　CORS_ALLOW_HEADERS = ["*"]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "authorization",
+    "content-type",
+    "x-csrftoken",
+    "x-requested-with"
+]
 
 ROOT_URLCONF = 'myproject.urls'
 
